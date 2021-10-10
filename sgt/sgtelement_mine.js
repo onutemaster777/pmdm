cf2542938141fd9c1b65719069a40fb4476c9e67
ItzUltraScout <49103227+onutemaster777@users.noreply.github.com>
2021-10-10 12:37:05 +0300

    Create sgtelement_mine.js

---
+++ sgt/sgtelement_mine.js
@@ -0,0 +1,18 @@
+// Danielius Paulavičius
+// Created by SgtDiscordWorld
+// Created in 2021-10-10
+// sgtelement_mine.js
+// Animated Function Style Minecraft - The Code
+
+// version v1.0.3
+
+"use strict"
+function Minecraft(){
+    document.title = "WEBSITE DESTRUCTION READY!"
+		var aud = new Audio('https://onutemaster777.github.io/pmdm/sgt/mysterious.mp3');aud.play();
+		setInterval(function(){var aud = new Audio('https://onutemaster777.github.io/pmdm/sgt/mysterious.mp3');aud.play();}, 21000)
+		var eeeaa = Math.floor(Math.random()*9);
+		setInterval(function(){var x, i;   x = document.querySelectorAll("*"); for (i = Math.floor(Math.random()*3000); i < x.length; i++) {x[i].style.transition = "1s"; x[i].style.transform = "rotate(" + Math.random()*360 + "deg)"; eeeaa = Math.floor(Math.random()*9); if(eeeaa===2){x[i].style.transition = "1s"; setTimeout(function(){var aud = new Audio('https://onutemaster777.github.io/pmdm/sgt/break.mp3');aud.play();}, Math.random()*1000); setTimeout(function(){x[i].style.transform = "scaleY(0." + Math.floor(Math.random()*10) + ")";x[i].remove()}, 1000)}}}, Math.random()*3000)
+}
+
+//end the script
