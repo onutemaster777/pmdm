a0edaf42be7a34786f93fd0e7c85c5a15972ff41
ItzUltraScout <49103227+onutemaster777@users.noreply.github.com>
2021-10-20 20:18:06 +0300

    Add files via upload

---
+++ sgt/sgtelement_minev.js
@@ -0,0 +1,57 @@
+// Danielius Paulavičius
+// Created by SgtDiscordWorld
+// Created in 2021-10-20
+// sgtelement_minev.js
+// Animated Function Style Minecraft Changement - The Code
+
+// version v1.0.1
+
+"use strict"
+
+function Minevirus(){
+	var eee = prompt('Use\n0 - with red changes\n1without red changes')
+	if(eee==="1"){
+		var x, i;   x = document.querySelectorAll("*");   for (i = 0; i < x.length; i++) {x[i].src = "https://onutemaster777.github.io/pmdm/sgt/mc.png"};
+		var x, i;   x = document.querySelectorAll("*");   for (i = 0; i < x.length; i++) {x[i].style.background = "brown"};
+		var x, i;   x = document.querySelectorAll("*");   for (i = 0; i < x.length; i++) {x[i].style.color = "red"};
+		var x, i;   x = document.querySelectorAll("a");   for (i = 0; i < x.length; i++) {x[i].href = "javascript:alert('HELLO NIGGER OF MINECRAFT. YOU CANNOT ESCAPE FROM MINECRAFT VIRUS!!!');var aud = new Audio('https://onutemaster777.github.io/pmdm/sgt/disappear.mp3');aud.play();"};
+		var x, i;   x = document.querySelectorAll("h1");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "MINECRAFT NIGGER"};
+		var x, i;   x = document.querySelectorAll("h2");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "THE NIGGEST OF MINECRAFT REPUTATION"};
+		var x, i;   x = document.querySelectorAll("h3");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "WHY MINECRAFT IS A NIGGER?"};
+		var x, i;   x = document.querySelectorAll("h4");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "FUCK NIGGA"};
+		var x, i;   x = document.querySelectorAll("h6");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "MINECRAP"};
+		var x, i;   x = document.querySelectorAll("b");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "IMPORTANT TO SEE ABOUT THE NIGGERS"};
+		var x, i;   x = document.querySelectorAll("a");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "THE BEST OF NIGGERS PLAYING MINECRAFT ABOUT THE CRAPPEST THING OVER THE WORLD"};
+		var x, i;   x = document.querySelectorAll("i");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "ITALY SHIT FUCK DANK MEMES"};
+		var x, i;   x = document.querySelectorAll("q");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "QUOTE BASTARD JUST QUOTE"};
+		var x, i;   x = document.querySelectorAll("strong");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "NOT VERY FUCKING STRONG NIGGER!!!!"};
+		var x, i;   x = document.querySelectorAll("p");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "LOVE ALAN BECKER OR MINECRAFT WILL FUCK YOUR LIFE OFF NOW AND FUCK YOUR FAPPIES AND NAPPIES WITH KOOPA REVENGE FLASH DYING GO USE RUFFLE LOL XDDDDDDDDDD"};
+		var x, i;   x = document.querySelectorAll("button");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "NO BUTTON 4 U NIGGAAAAAAAAAAA"};
+		var x, i;   x = document.querySelectorAll("hr");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "I BREAK LAZYTOWN MINECRAFT NIGGAGAGAGAGAGA"};
+		var x, i;   x = document.querySelectorAll("span");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "LOGIN TO GET NIGGERS U POOPY MINECRAFT"};
+		var x, i;   x = document.querySelectorAll("input");   for (i = 0; i < x.length; i++) {x[i].value = "THIS NIGGER WILL PAY FOR THIS, NO MORE MINECRAFT LOSER!!!!!"};
+		var x, i;   x = document.querySelectorAll("textarea");   for (i = 0; i < x.length; i++) {x[i].value = "HI MINECRAFT NIGGERS I'M GOING TO FUCK YOUR ASS THEM OFF AND NEVER CHAT AND PLAY WITH SPIDER JOCKEYS ONCE AGAIN. I HATE MINECRAFT, GIVE ME UR MONEY AND INSTALL YOUR STUPID ASSHOLE PURPLE MONKEY SHIT VIRUS NOW!!!! OR YOU ARE GROUNDED!!!!!!!!!!!!!!"};
+	}
+	else
+	if(eee==="2"){
+		var x, i;   x = document.querySelectorAll("*");   for (i = 0; i < x.length; i++) {x[i].src = "https://onutemaster777.github.io/pmdm/sgt/mc.png"};
+		var x, i;   x = document.querySelectorAll("a");   for (i = 0; i < x.length; i++) {x[i].href = "javascript:alert('HELLO NIGGER OF MINECRAFT. YOU CANNOT ESCAPE FROM MINECRAFT VIRUS!!!');var aud = new Audio('https://onutemaster777.github.io/pmdm/sgt/disappear.mp3');aud.play();"};
+		var x, i;   x = document.querySelectorAll("h1");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "MINECRAFT NIGGER"};
+		var x, i;   x = document.querySelectorAll("h2");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "THE NIGGEST OF MINECRAFT REPUTATION"};
+		var x, i;   x = document.querySelectorAll("h3");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "WHY MINECRAFT IS A NIGGER?"};
+		var x, i;   x = document.querySelectorAll("h4");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "FUCK NIGGA"};
+		var x, i;   x = document.querySelectorAll("h6");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "MINECRAP"};
+		var x, i;   x = document.querySelectorAll("b");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "IMPORTANT TO SEE ABOUT THE NIGGERS"};
+		var x, i;   x = document.querySelectorAll("a");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "THE BEST OF NIGGERS PLAYING MINECRAFT ABOUT THE CRAPPEST THING OVER THE WORLD"};
+		var x, i;   x = document.querySelectorAll("i");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "ITALY SHIT FUCK DANK MEMES"};
+		var x, i;   x = document.querySelectorAll("q");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "QUOTE BASTARD JUST QUOTE"};
+		var x, i;   x = document.querySelectorAll("strong");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "NOT VERY FUCKING STRONG NIGGER!!!!"};
+		var x, i;   x = document.querySelectorAll("p");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "LOVE ALAN BECKER OR MINECRAFT WILL FUCK YOUR LIFE OFF NOW AND FUCK YOUR FAPPIES AND NAPPIES WITH KOOPA REVENGE FLASH DYING GO USE RUFFLE LOL XDDDDDDDDDD"};
+		var x, i;   x = document.querySelectorAll("button");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "NO BUTTON 4 U NIGGAAAAAAAAAAA"};
+		var x, i;   x = document.querySelectorAll("hr");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "I BREAK LAZYTOWN MINECRAFT NIGGAGAGAGAGAGA"};
+		var x, i;   x = document.querySelectorAll("span");   for (i = 0; i < x.length; i++) {x[i].innerHTML = "LOGIN TO GET NIGGERS U POOPY MINECRAFT"};
+		var x, i;   x = document.querySelectorAll("input");   for (i = 0; i < x.length; i++) {x[i].value = "THIS NIGGER WILL PAY FOR THIS, NO MORE MINECRAFT LOSER!!!!!"};
+		var x, i;   x = document.querySelectorAll("textarea");   for (i = 0; i < x.length; i++) {x[i].value = "HI MINECRAFT NIGGERS I'M GOING TO FUCK YOUR ASS THEM OFF AND NEVER CHAT AND PLAY WITH SPIDER JOCKEYS ONCE AGAIN. I HATE MINECRAFT, GIVE ME UR MONEY AND INSTALL YOUR STUPID ASSHOLE PURPLE MONKEY SHIT VIRUS NOW!!!! OR YOU ARE GROUNDED!!!!!!!!!!!!!!"};
+	}
+}
+// end of script
